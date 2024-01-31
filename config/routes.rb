dde6f8f820e1ebec163911c79bb6e99ca2d1d8ab
Takuya Kinoshita <kktak02@gmail.com>
2024-02-01 07:49:16 +0900

    add users to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
   get '/help', to: "static_pages#help"
   get '/home', to: "static_pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users
 end
